2f4f8b541b8cdbcb0bdf6b2d8c69622363e63c64
Jakub Kosinski <jakub.kosinski@ghandal.net>
2009-09-12 00:59:42 +0200

    fixed unitialized variable in s3 provider

--- lib/backup_fu/s3.rb
+++ lib/backup_fu/s3.rb
@@ -7,7 +7,7 @@ module BackupFu
     def initialize(options={})
       check_config(options)
       @s3 = RightAws::S3.new(@options[:access_key_id], @options[:secret_access_key])
-      @s3_bucket = s3.bucket(@options[:bucket], true, 'private')
+      @s3_bucket = @s3.bucket(@options[:bucket], true, 'private')
     end
 
     def delete(file)
@@ -36,7 +36,7 @@ module BackupFu
       @options[:secret_access_key] = ENV['AMAZON_SECRET_ACCESS_KEY'] || options[:aws_secret_access_key]
       raise(S3ConfigError, "AWS Access Key ID or AWS Secret Key not set in config/backup_fu.yml. If you do not want to user AWS S3 provider, change provider in config/backup_fu.yml to ftp or local") if @options[:access_key_id].blank? || @options[:access_key_id].include?('--replace me') || @options[:secret_access_key].include?('--replace me')
 
-      @options[:bucket] = ENV['s3_bucket'] || options[:bucket]
+      @options[:bucket] = ENV['s3_bucket'] || options[:s3_bucket] || options[:bucket]
       raise(S3ConfigError, "S3 bucket (s3_bucket) not set in config/backup_fu.yml. This bucket must be created using an external S3 tool like S3 Browser for OS X, or JetS3t (Java-based, cross-platform).") if @options[:bucket].blank? || @options[:bucket] == "some-s3-bucket"
 
     end
